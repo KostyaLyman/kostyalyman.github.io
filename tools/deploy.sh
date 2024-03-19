4da52dd87578b382402e2970c110504efe178499
KostyaLyman <lyman.kostiantyn@gmail.com>
2024-03-19 01:03:00 -0700

    reinstall chirpy: deploy sh

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -18,6 +18,17 @@ _config="_config.yml"
 
 _no_pages_branch=false
 
+# CLI Dependencies
+CLI=("git" "npm")
+
+ACTIONS_WORKFLOW=pages-deploy.yml
+
+# temporary file suffixes that make `sed -i` compatible with BSD and Linux
+TEMP_SUFFIX="to-delete"
+
+_no_gh=false
+
+
 _backup_dir="$(mktemp -d)"
 
 _baseurl=""
@@ -43,6 +54,89 @@ init() {
   fi
 
   _baseurl="$(grep '^baseurl:' _config.yml | sed "s/.*: *//;s/['\"]//g;s/#.*//")"
+}
+
+
+# BSD and GNU compatible sed
+_sedi() {
+  regex=$1
+  file=$2
+  sed -i.$TEMP_SUFFIX "$regex" "$file"
+  rm -f "$file".$TEMP_SUFFIX
+}
+
+_check_cli() {
+  for i in "${!CLI[@]}"; do
+    cli="${CLI[$i]}"
+    if ! command -v "$cli" &>/dev/null; then
+      echo "Command '$cli' not found! Hint: you should install it."
+      exit 1
+    fi
+  done
+}
+
+_check_status() {
+  if [[ -n $(git status . -s) ]]; then
+    echo "Error: Commit unstaged files first, and then run this tool again."
+    exit 1
+  fi
+}
+
+_check_init() {
+  local _has_inited=false
+
+  if [[ ! -d .github ]]; then # using option `--no-gh`
+    _has_inited=true
+  else
+    if [[ -f .github/workflows/$ACTIONS_WORKFLOW ]]; then
+      # on BSD, the `wc` could contains blank
+      local _count
+      _count=$(find .github/workflows/ -type f -name "*.yml" | wc -l)
+      if [[ ${_count//[[:blank:]]/} == 1 ]]; then
+        _has_inited=true
+      fi
+    fi
+  fi
+
+  if $_has_inited; then
+    echo "Already initialized."
+    exit 0
+  fi
+}
+
+check_env() {
+  _check_cli
+  _check_status
+  _check_init
+}
+
+checkout_latest_release() {
+  hash=$(git log --grep="chore(release):" -1 --pretty="%H")
+  git reset --hard "$hash"
+}
+
+init_files() {
+  if $_no_gh; then
+    rm -rf .github
+  else
+    ## Change the files of `.github`
+    mv .github/workflows/$ACTIONS_WORKFLOW.hook .
+    rm -rf .github
+    mkdir -p .github/workflows
+    mv ./${ACTIONS_WORKFLOW}.hook .github/workflows/${ACTIONS_WORKFLOW}
+
+    ## Cleanup image settings in site config
+    _sedi "s/^img_cdn:.*/img_cdn:/;s/^avatar:.*/avatar:/" _config.yml
+  fi
+
+  # remove the other files
+  rm -rf _posts/*
+
+  # build assets
+  npm i && npm run build
+
+  # track the js output
+  _sedi "/^assets.*\/dist/d" .gitignore
 }
 
 build() {
@@ -117,6 +211,9 @@ deploy() {
 
 main() {
   init
+  check_env
+  checkout_latest_release
+  init_files
   build
   test
   resume_site_dir
